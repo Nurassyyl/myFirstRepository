4060113922e9c64ec817795aaa7bd20d7be1909e
Nurassyl Abdildabek <abdildabekn@mail.ru>
2022-07-29 23:38:30 +0600

    создал новое переменное и закончил задание второго урока

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let title = "title";
-let screens = "screens";
-let screenPrice = "screenPrice";
-let rollback = "rollback";
-let fullPrice = "fullPrice";
-let adaptive = "adaptive";
+let title = "My first repository";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 123;
+let rollback = 99;
+let fullPrice = 10000000000;
+let adaptive = true;
 
-alert("Hello world");
-console.log("Nurassyl");
+console.log(typeof title);
+console.log(typeof screens);
+console.log(typeof screenPrice);
+console.log(screens.length)
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей/ долларов/гривен/юани");
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей / долларов / гривен / юани");
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+console.log(fullPrice * (rollback / 100));
