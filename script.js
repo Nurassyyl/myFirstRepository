2633f3295ffdda04c7d185e85c24aa2c85ff9dbf
Nurassyl Abdildabek <abdildabekn@mail.ru>
2022-08-02 19:00:57 +0600

    Первое задание выполнил

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-let title = "title";
-let screens = "screens";
-let screenPrice = "screenPrice";
-let rollback = "rollback";
-let fullPrice = "fullPrice";
-let adaptive = "adaptive";
+"use strict";
+// до 6
+let title = prompt("Как называется ваш проект");
+console.log(title);
+let screens = prompt("Какие типы экранов нужно разработать?");
+console.log(screens);
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+console.log(screenPrice);
+let adaptive = prompt("Нужен ли адаптив на сайте?");
+console.log(adaptive);
 
-alert("Hello world");
-console.log("Nurassyl");
+// 7
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service1);
+let servicePrice1 = +prompt("Сколько это будеть стоить?");
+console.log(servicePrice1);
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service2);
+let servicePrice2 = +prompt("Сколько это будеть стоить?");
+console.log(servicePrice2);
+
+// 8
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+console.log(fullPrice);
+
+// 9
+// let servicePercentPrice =
+
+// 10
+if (fullPrice >= 30000) {
+    console.log("Даем скидку 10%");
+} else if (fullPrice > 15000 && fullPrice < 30000) {
+    console.log("Даем скидку 5%");
+} else if (fullPrice > 0 && fullPrice < 15000) {
+    console.log("Скидка не предусмотрена");
+} else {
+    console.log("Что-то пошло не так");
+}
