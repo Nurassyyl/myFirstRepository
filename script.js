19e92537e9b243bd9b98c88a1acc79baac62c4f1
unknown <you@example.com>
2022-08-05 02:59:30 +0600

    Комментарий убрал

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 "use strict";
-// до 6
-// let title = prompt("Как называется ваш проект");
-// console.log(title);
-// let screens = prompt("Какие типы экранов нужно разработать?");
-// console.log(screens);
-// let screenPrice = +prompt("Сколько будет стоить данная работа?");
-// console.log(screenPrice);
-// let adaptive = prompt("Нужен ли адаптив на сайте?");
-// console.log(adaptive);
+до 6
+let title = prompt("Как называется ваш проект");
+console.log(title);
+let screens = prompt("Какие типы экранов нужно разработать?");
+console.log(screens);
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+console.log(screenPrice);
+let adaptive = prompt("Нужен ли адаптив на сайте?");
+console.log(adaptive);
 
-// // 7
-// let service1 = prompt("Какой дополнительный тип услуги нужен?");
-// console.log(service1);
-// let servicePrice1 = +prompt("Сколько это будеть стоить?");
-// console.log(servicePrice1);
-// let service2 = prompt("Какой дополнительный тип услуги нужен?");
-// console.log(service2);
-// let servicePrice2 = +prompt("Сколько это будеть стоить?");
-// console.log(servicePrice2);
+// 7
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service1);
+let servicePrice1 = +prompt("Сколько это будеть стоить?");
+console.log(servicePrice1);
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service2);
+let servicePrice2 = +prompt("Сколько это будеть стоить?");
+console.log(servicePrice2);
 
-// const getAllServicePrices = function(servicePrice1, servicePrice2) {
-//     return servicePrice1 + servicePrice2;
-// }
-// const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-// console.log(allServicePrices);
+const getAllServicePrices = function(servicePrice1, servicePrice2) {
+    return servicePrice1 + servicePrice2;
+}
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+console.log(allServicePrices);
 
-// function getFullPrice(screenPrice, allServicePrices) {
-//     return screenPrice + allServicePrices;
-// }
+function getFullPrice(screenPrice, allServicePrices) {
+    return screenPrice + allServicePrices;
+}
 
-// const fullPrice = getFullPrice(screenPrice, allServicePrices);
-// console.log(fullPrice);
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
+console.log(fullPrice);
 let title = " КаЛьКулятор Верстки";
 console.log(title.toLowerCase());
 
@@ -41,13 +41,13 @@ const getTitle = function (title) {
 getTitle(" КаЛьКулятор Верстки");
 
 // 10
-// if (fullPrice >= 30000) {
-//     console.log("Даем скидку 10%");
-// } else if (fullPrice => 15000 && fullPrice <= 30000) {
-//     console.log("Даем скидку 5%");
-// } else if (fullPrice => 0 && fullPrice <= 15000) {
-//     console.log("Скидка не предусмотрена");
-// } else {
-//     console.log("Что-то пошло не так");
-// }
+if (fullPrice >= 30000) {
+    console.log("Даем скидку 10%");
+} else if (fullPrice => 15000 && fullPrice <= 30000) {
+    console.log("Даем скидку 5%");
+} else if (fullPrice => 0 && fullPrice <= 15000) {
+    console.log("Скидка не предусмотрена");
+} else {
+    console.log("Что-то пошло не так");
+}
 
